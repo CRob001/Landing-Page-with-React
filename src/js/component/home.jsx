08e065e46743aac77724165b5c4c7a824ae9214f
Cat <126123826+CRob001@users.noreply.github.com>
2023-05-07 14:48:55 +0000

    trying to figure out card text component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,14 +9,14 @@ import Footer from "./Footer";
 //create your first componenmt
 function Home() {
 	return (
-		<div className="">
+		<div className= "">
 			<Navbar/>
 			<Jumbotron/>
 			<div className="cards">
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
+				<Card title={"First Card"} content={"This is a sentance including some card content."} />
+				<Card title={"Second Card"} content={"This is MORE content on a second card."} />
+				<Card title={"Third Card"} content={"This is EVEN MORE content on a third card."} />
+				<Card title={"Fourth Card"} content={"This is WOW EVEN MORE content on a fourth card."} />
 			</div>
 			<Footer/>
 		</div>
